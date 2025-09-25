Fixed bug in v0.1.6 that arose from calculating eta_ap in the code, which messed up the calculation of eta_fwd. Also cleaned up some function names.

tifuun_sensitivity/cascade.py
@@ -5,7 +5,7 @@ import csv
 
 from typing import List, Union, Dict, Tuple
 
-from .instruments import eta_Al_ohmic_850, photon_NEP_kid, window_trans
+from .instruments import eta_Al_ohmic_850, window_trans
 from .physics import johnson_nyquist_psd
 
 def eta_Al_ohmic(F_sky: np.ndarray) -> np.ndarray: 

tifuun_sensitivity/instruments.py
@@ -56,35 +56,28 @@ def eta_ruze(F: ArrayLike,
     return np.exp(-((4.0 * np.pi * sigma * F / c) ** 2.0))
 
 
-def photon_NEP_kid(F: ArrayLike, 
+def photon_NEP2_kid(F: ArrayLike, 
                    psd_KID: ArrayLike) -> ArrayLike:
-    """NEP of the KID, with respect to the absorbed power.
+    """NEP squared of the KID, with respect to the absorbed power.
+    Note that the square of the NEP is returned, as this quantity is more convenient for the calculation.
+    Also, we do not multiply by dF here, as we do that further downstream outside of the integral.
 
     Parameters
     -----------
     F
         Frequency of the signal responsible for loading. Units: Hz.
-    psd_kid
+    psd_KID
         Power spectral density entering MKID the KID. Units: W / Hz.
-    filterbank
-        Detection bandwidth, with respect to the power that sets the loading. Units: Hz.
 
     Returns
     -------
-    NEP_kid
-        Noise-equivalent power of the KID.
-
-    Notes
-    -----
-    Pkid/(W_F * h * F) gives the occupation number.
-
+    NEP2_kid
+        Noise-equivalent power squared of the KID.
     """
-    # photon_term = 2 * Pkid * (h*F + Pkid/W_F)
     poisson_term = 2 * psd_KID * h * F
     bunching_term = 2 * psd_KID**2
     r_term = 4 * Delta_Al * psd_KID / eta_pb
     return poisson_term + bunching_term + r_term
-
 
 def window_trans(
     F: ArrayLike,

tifuun_sensitivity/simulator.py
@@ -9,7 +9,7 @@ from typing import List, Union, Dict
 import numpy as np
 import pandas as pd
 from .atmosphere import eta_atm_func
-from .instruments import eta_Al_ohmic_850, photon_NEP_kid, window_trans, average_over_filterbank, eta_ruze
+from .instruments import eta_Al_ohmic_850, photon_NEP2_kid, window_trans, average_over_filterbank, eta_ruze
 from .physics import johnson_nyquist_psd, rad_trans, T_from_psd, c, h, k
 from .filterbank import generateFilterbankFromR
 
@@ -158,14 +158,14 @@ def calculator(
         if isinstance(psd_stage, str):
             psd_stage = (1 - eta_atm) * psd_atm
             branch_fwd += 1 
+        
+        if use_for_eta_ap[idx]:
+            eta_ap *= eta_stage     
 
         if use_for_eta_inst[idx]:
             # If stage is inside cryostat (including window), it counts towards eta_inst
             eta_inst *= eta_stage 
         
-        if use_for_eta_ap[idx]:
-            eta_ap *= eta_stage 
-            
             # Also check, if this is first stage inside instrument -> assign to eta_window
             if eta_window_set == False:
                 eta_window = average_over_filterbank(eta_stage, filterbank)
@@ -224,7 +224,7 @@ def calculator(
     # For eta_sw, also smooth eta_atm over filter response
     eta_atm = average_over_filterbank(eta_atm, filterbank) 
 
-    NEP = np.sqrt(np.nansum(photon_NEP_kid(F_sky[:,None], psd_running), axis=0) * dF_sky) * KID_excess_noise_factor
+    NEP = np.sqrt(np.nansum(photon_NEP2_kid(F_sky[:,None], psd_running), axis=0) * dF_sky) * KID_excess_noise_factor
     NEP_inst = NEP / eta_inst  # Instrument NEP    
 
     # ##############################################################